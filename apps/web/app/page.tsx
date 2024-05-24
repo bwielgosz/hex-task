51bd20f618e0bf56b2541917d68c7302955afd26
Bartlomiej Wielgosz <bartlomiej.wielgosz@intive.com>
2024-05-24 11:00:54 +0200

    feat(web): add cards section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,45 @@
+import data from "./data.json"
+import Image from "next/image"
+import { buttonVariants } from "@repo/design-system/button.tsx"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@repo/design-system/card.tsx"
+import Link from "next/link"
+
 export default function Page(): JSX.Element {
-  return <main className="flex-1 mt-8 flex flex-col items-center"></main>
+  return (
+    <main className="flex-1 mt-8 flex flex-col items-center px-4">
+      <section className="grid grid-cols-3 gap-4">
+        {data.tiles.map((tile) => (
+          <Card key={tile.id}>
+            <CardHeader>
+              <CardTitle>{tile.title}</CardTitle>
+              <CardDescription>{tile.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Image
+                src={tile.image}
+                width={200}
+                height={200}
+                alt="tile image"
+              />
+            </CardContent>
+            <CardFooter>
+              <Link
+                href="/"
+                className={buttonVariants({ className: "w-full" })}
+              >
+                {tile.buttonText}
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
+    </main>
+  )
 }
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+      },
+    ],
+  },
   transpilePackages: ["@repo/design-system"],
-};
+}

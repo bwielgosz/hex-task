db1b3b4c704df24c00cb11ac402181dbc33fd381
Bartlomiej Wielgosz <bartlomiej.wielgosz@intive.com>
2024-05-24 11:19:37 +0200

    feat(web): add newsletter section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import data from "./data.json"
 import Image from "next/image"
 import { buttonVariants } from "@repo/design-system/button.tsx"
+import { Newsletter } from "@repo/design-system/newsletter.tsx"
 import {
   Card,
   CardContent,
@@ -13,7 +14,7 @@ import Link from "next/link"
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex-1 mt-8 flex flex-col items-center px-4">
+    <main className="flex-1 mt-12 flex flex-col items-center px-4 gap-12">
       <section className="grid grid-cols-3 gap-4">
         {data.tiles.map((tile) => (
           <Card key={tile.id}>
@@ -39,6 +40,14 @@ export default function Page(): JSX.Element {
             </CardFooter>
           </Card>
         ))}
+      </section>
+      <section>
+        <Newsletter
+          title={data.newsletter.title}
+          description={data.newsletter.description}
+          placeholder={data.newsletter.inputPlaceholder}
+          buttonText={data.newsletter.buttonText}
+        />
       </section>
     </main>
   )

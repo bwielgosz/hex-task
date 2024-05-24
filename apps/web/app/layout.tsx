7e96975d812d76a5db193d971c01e1f1716091dc
Bartlomiej Wielgosz <bartlomiej.wielgosz@intive.com>
2024-05-24 10:10:52 +0200

    feat: initial commit 🚀

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css"
+import "@repo/design-system/global.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Hex task",
+  description: "Just a fun task",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
